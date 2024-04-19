65b91d52737020ae8abdafe4cc8848a2355e7823
Nirex <akay93796@gmail.com>
2024-04-20 00:08:09 +0530

    fix user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,30 +1,28 @@
 import { User } from "../models/user.models.js";
 import asyncHandler from "../utils/asyncHandler.js"
-import ApiError from "../utils/apiError.js"
 import ApiResponse from "../utils/apiResponse.js"
-import {upploadOnCloudinary} from "../utils/FileUpload.js"
+import { upploadOnCloudinary } from "../utils/FileUpload.js"
+import { sendEmail } from "../utils/sendEmail.js";
 
 const genrateRefershToken = async (userId) => {
     try {
         const user = await User.findById(userId);
 
         const refershToken = user.genrateRefershToken();
-        console.log("token");
 
         user.refershToken = refershToken;
-        await user.save({ validateBeforeSave: false});
+        await user.save({ validateBeforeSave: false });
         return refershToken;
 
     } catch (error) {
-        throw new ApiError(500, "Something went worng during create token!")
+        return res.status(500).json(new ApiResponse(500, "Something went worng during create token!"))
     }
 };
+
 
 /*------------------------------ 
     Signup : /api/v1/auth/signup
 ------------------------------*/
-
-
 const signUpUser = asyncHandler(async (req, res) => {
 
     const { username, email, password } = req.body;
@@ -32,7 +30,7 @@ const signUpUser = asyncHandler(async (req, res) => {
     const existedUser = await User.findOne({ email });
 
     if (existedUser) {
-        throw new ApiError(400, "Email already exist!",);
+        return res.status(400).json(new ApiResponse(400, "Email already exist!"));
     }
 
     const createdUser = await User.create({
@@ -44,7 +42,7 @@ const signUpUser = asyncHandler(async (req, res) => {
     const user = await User.findById(createdUser._id).select("-password -refershToke");
 
     if (!user) {
-        throw new ApiError(400, "Error occured during Register");
+        return res.status(400).json(new ApiResponse(400, "Error occured during Register"));
     };
 
 
@@ -59,20 +57,19 @@ const signUpUser = asyncHandler(async (req, res) => {
 /*------------------------------ 
     login : /api/v1/auth/login
 ------------------------------*/
-
 const logInUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
     if (!user) {
-        throw new ApiError(400, "Invalid User Credential! ");
+        return res.status(400).json(new ApiResponse(400, "Invalid User Credential! "));
     }
 
     const isPasswordCorrect = await user.isPasswordCorrect(password);
 
     if (!isPasswordCorrect) {
-        throw new ApiError(401, "Invalid User Credentials!")
+        return res.status(401).json(new ApiResponse(401, "Invalid User Credential! "));
     }
 
     const refershToken = await genrateRefershToken(user._id);
@@ -83,6 +80,8 @@ const logInUser = asyncHandler(async (req, res) => {
         httpOnly: true,
         secure: true
     }
+
+    await sendEmail(user.username);
 
     return res
         .status(200)
@@ -97,88 +96,93 @@ const logInUser = asyncHandler(async (req, res) => {
 
             )
         )
-
-
-
-
-
 })
+
+
+/*------------------------------ 
+    Logout : /api/v1/auth/logout
+------------------------------*/
+const logOutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refershToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("refershToken", options)
+        .json(
+            new ApiResponse(200, "Logout Successfully", {})
+        );
+})
+
 
 /*------------------------------ 
     Logout : /api/v1/auth/logout
 ------------------------------*/
 
-const logOutUser = asyncHandler(async (req, res) =>{
-    await User.findByIdAndUpdate(
-        req.user._id,
-        {
-            $unset : {
-                refershToken : 1
-            }
-        },
-        {
-            new : true
-        }
-    )
-
-    const options = {
-        httpOnly : true,
-        secure : true
-    }
-
-    return res
-    .status(200)
-    .clearCookie("refershToken", options)
-    .json(
-        new ApiResponse(200, "Logout Successfully", {})
-    );
+const searchUser = asyncHandler(async (req, res) => {
 })
 
-// /*------------------------------ 
-//     Logout : /api/v1/auth/logout
-// ------------------------------*/
-
-// const searchUser = asyncHandler(async (req, res) => {
-//     // todo
-// })
 
 /*------------------------------ 
     UpdateProfilePic : /api/v1/user/updateprofilepic
 ------------------------------*/
 const updateprofilepic = asyncHandler(async (req, res) => {
-    
+
     const avatarLocalPath = req.file?.path;
 
-    if(!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required");
+    if (!avatarLocalPath) {
+        return res.status(400).json(new ApiResponse(400, "Avatar file is required"));
     }
 
     // upload them to cloudinary
     const avatar = await upploadOnCloudinary(avatarLocalPath);
 
-    if(!avatar){
-        throw new ApiError(400, "Avatar file is required");
+    if (!avatar) {
+        return res.status(400).json(new ApiResponse(400, "Avatar file is required"));
     }
 
     // find loged in user
-    const user = await User.findById(req.user._id); 
+    const user = await User.findById(req.user._id);
 
-    if(!user) {
-        throw new ApiError(400, "Login required!");
+    if (!user) {
+        return res.status(400).json(new ApiResponse(400, "Login Required!"));
     }
 
     // save avatar in database
     user.avatar = avatar.url;
-    await user.save({validateBeforeSave : false});
+    await user.save({ validateBeforeSave: false });
     return res.status(200).json(new ApiResponse(200, "Profile pic Update successfully."));
+});
 
-})
+/*------------------------------ 
+    ForgotPassword : /api/v1/auth/forgotpassword
+------------------------------*/
+const forgotpassword = asyncHandler(async (req, res) =>{
+
+});
+
+
 
 export {
     signUpUser,
     logInUser,
     logOutUser,
-    // searchUser,
+    searchUser,
     updateprofilepic,
+    forgotpassword,
 
 }
